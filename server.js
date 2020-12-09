797c27e388f063ab2a9d517c6db3384f693c1cfd
Abel Masila <abelmasila@gmail.com>
2020-12-09 19:09:40 +0300

    get ready for prod

--- server.js
+++ server.js
@@ -22,6 +22,13 @@ if (process.env.NODE_ENV === "development") {
 
 app.use("/api/v1/transactions", transactions);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(
