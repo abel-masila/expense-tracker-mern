a4ac6bc722791b2481983b3b023c117a1249c2d7
Abel Masila <abelmasila@gmail.com>
2020-12-09 18:05:54 +0300

    get all transactions

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,17 +1,31 @@
+const Transaction = require("./../models/Transactions");
+
 // @desc GET all transactions
 // @route GET /api/v1/transactions
-exports.getTransactions = (req, res, next) => {
-  res.send("GET Txns");
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions,
+    });
+  } catch (error) {
+    return res.send(500).json({
+      success: false,
+      error: "Server Error",
+    });
+  }
 };
 
 // @desc Add  transaction
 // @route POST /api/v1/transactions
-exports.addTransaction = (req, res, next) => {
+exports.addTransaction = async (req, res, next) => {
   res.send("POST Txns");
 };
 
 // @desc DELETE  transaction
 // @route DELETE /api/v1/transactions/:id
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
   res.send("DELETE Txns");
 };
